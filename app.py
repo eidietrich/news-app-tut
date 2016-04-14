c2cecea6247e9f6a5eeef148f51f1d8645782ec9
Eric Dietrich <eric.dietrich@gmail.com>
2016-04-14 16:23:14 -0600

    Add templates

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import flask
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    template = 'index.html'
+    return render_template(template)
 
 if __name__ == '__main__':
     # Fire up Flask test server
